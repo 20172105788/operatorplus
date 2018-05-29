d45616c87c790c88f9684c1e6f4e038d8b2795e6
20172105788 <r1403633075@163.com>
2018-05-29 15:35:25 +0800

    将英尺转化为英寸计算再转化为英尺并输出
    还是有负的英寸输出

--- operatorplus/operatorplus.cpp
+++ operatorplus/operatorplus.cpp
@@ -12,23 +12,26 @@ private:
 public:
 	void setvalue(int ft,int in);
 	void display();
-	CFeet add(CFeet & objf);
+	CFeet minus(CFeet & objf);
 	CFeet operator -(CFeet &objf);
-
 };
 void CFeet::setvalue(int ft, int in)
 {
 	feet = ft + in / 12;
 	inches = in % 12;
+	inches = 12 * ft + in;
+	feet = ft+ in / 12;
+	inches = in % 12;
 }
 void CFeet::display()
 {
+	cout << inches << "Ó¢´ç" << endl;
 	cout << feet << "Ó¢³ß" << inches << "Ó¢´ç" << endl;
 }
-CFeet CFeet::add(CFeet & objf)
+CFeet CFeet::minus(CFeet & objf)
 {
 	CFeet temp;
-	temp.setvalue(feet - objf.feet, inches - objf.inches);
+	temp.setvalue(feet - objf.feet,inches - objf.inches);
 	temp.display();
 	return temp;
 }
@@ -43,10 +46,10 @@ CFeet CFeet::operator -(CFeet &objf)
 int main()
 {
 	CFeet A,B,C;
-	A.setvalue(-10, -11);
-	B.setvalue(10,-11);
+	A.setvalue(1,2);
+	B.setvalue(2,2);
 	C = A - B;
 	C.display();
-    return 0;
-}
+     return 0;
+}  
 
